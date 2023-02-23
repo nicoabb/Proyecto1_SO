766c135c39927452ea43aac875ff9f9334640dc2
nicoabb <nicolas.briceno@correo.unimet.edu.ve>
2023-02-22 23:57:55 -0400

    ProjectManagerTLOU now working

--- Proyecto1_Briceno_Ferrer/src/Classes/ProjectManagerTLOU.java
+++ Proyecto1_Briceno_Ferrer/src/Classes/ProjectManagerTLOU.java
@@ -5,31 +5,78 @@
  */
 package Classes;
 
+import Interfaces.Dashboard;
 import java.util.concurrent.Semaphore;
 
 /**
  *
  * @author Nicolás Briceño
  */
-public class ProjectManagerTLOU {
+public class ProjectManagerTLOU extends Thread{
     
     private int dayDuration;
-    private int counter; //Días para la comparación
+    private boolean stop;
     private Semaphore countMutex; 
-    private int countChangeTime; //Tiempo que tarda el PM en cambiar el contador
     private int remainderTime;  //Cantidad de horas para alternar entre ver Rick y Morty y revisar sprints reviews
     private String state; //Estado del PM (RyM o sprints reviews)
     private Semaphore stateMutex; //Mutex del estado, para que no se cambie cuando el director lea
     
-    public ProjectManagerTLOU(int dayDuration, int counter, Semaphore countMutex, int countChangeTime, int remainderTime) {
-        this.dayDuration = dayDuration;
-        this.counter = counter;
+    public ProjectManagerTLOU(boolean stop, int dayDuration, Semaphore countMutex, Semaphore stateMutex) {
+        this.dayDuration = (dayDuration * 1000);
+        this.stop = stop;
         this.countMutex = countMutex;
-        this.countChangeTime = countChangeTime;
-        this.remainderTime = dayDuration - countChangeTime;
+        this.stateMutex = stateMutex;
+        this.remainderTime = this.dayDuration - 1000;
+        
     }
     
-    
+    @Override
+    public void run() {
+        while(!stop){
+            try {
+                //Project Manager cambia el contador
+                
+                countMutex.acquire();
+                
+                stateMutex.acquire();
+                state = "Cambiando el contador";
+                Dashboard.pmState.setText(state);
+                stateMutex.release();
+                
+                Dashboard.counter--;
+                Dashboard.daysCounter.setText(Integer.toString(Dashboard.counter));
+                Thread.sleep(1000);
+                
+                countMutex.release();
+                
+                while(remainderTime >= 0){
+                    
+                    stateMutex.acquire();
+                    state = "Viendo Rick y Morty";
+                    Dashboard.pmState.setText(state);
+                    stateMutex.release();
+                    
+                    Thread.sleep(250); //1000 ms es 1 hora, 1/4 de hora son 15 minutos, de ahí los 250 ms
+                    remainderTime -= 250;
+                    
+                    stateMutex.acquire();
+                    state = "Revisando sprints reviews";
+                    Dashboard.pmState.setText(state);
+                    stateMutex.release();
+                    
+                    Thread.sleep(250);
+                    remainderTime -= 250;
+                    
+                }
+                
+                remainderTime = dayDuration - 1000;
+
+            } catch (InterruptedException ex) {
+
+            }
+        
+        }
+    }
     
     
 }
--- Proyecto1_Briceno_Ferrer/src/Interfaces/Dashboard.java
+++ Proyecto1_Briceno_Ferrer/src/Interfaces/Dashboard.java
@@ -11,6 +11,7 @@ import Classes.ProdBegTLOU;
 import Classes.ProdCreditTLOU;
 import Classes.ProdIntroTLOU;
 import Classes.ProdPlotTLOU;
+import Classes.ProjectManagerTLOU;
 import java.util.concurrent.Semaphore;
 
 /**
@@ -19,9 +20,10 @@ import java.util.concurrent.Semaphore;
  */
 public class Dashboard extends javax.swing.JFrame {
     
-    private boolean start;
+    private boolean stop;
     private int dayDuration;
     private int maxProdsTLOU; //Cantidad máxima de productores TLOU
+    public static volatile int counter; //Contador del número de días restantes para el corte de comparación
     
     //Espacio en Drive
     public static volatile int introDriveTLOU = 0;
@@ -60,6 +62,10 @@ public class Dashboard extends javax.swing.JFrame {
     private AssemblerTLOU assemblerTLOU;
     private Semaphore mutexAssembler;
     
+    //Datos del Project Manager
+    private ProjectManagerTLOU pmTLOU;
+    private Semaphore stateMutexTLOU, countMutexTLOU;
+    
     /**
      * Creates new form Dashboard
      */
@@ -70,7 +76,9 @@ public class Dashboard extends javax.swing.JFrame {
     
     public void inicializarTLOU() {
         //Con el JSON tienen que cambiarse los valores de abajo
+        this.stop = false;
         this.dayDuration = 2;
+        this.counter = 30;
         this.introMaxDriveTLOU = 30;
         this.creditMaxDriveTLOU = 25;
         this.begMaxDriveTLOU = 50;
@@ -116,6 +124,13 @@ public class Dashboard extends javax.swing.JFrame {
         this.mutexAssembler = new Semaphore(1);
         this.assemblerTLOU = new AssemblerTLOU(dayDuration, mutexAssembler, mutexIntroTLOU, semIntroTLOU, semAssemIntroTLOU, mutexBegTLOU, semBegTLOU, semAssemBegTLOU, mutexEndTLOU, semEndTLOU, semAssemEndTLOU, mutexCreditTLOU, semCreditTLOU, semAssemCreditTLOU, mutexPlotTLOU, semPlotTLOU, semAssemPlotTLOU);
         assemblerTLOU.start();
+        
+        //Creando Project Manager TLOU
+        this.stateMutexTLOU = new Semaphore(1);
+        this.countMutexTLOU = new Semaphore(1);
+        this.pmTLOU = new ProjectManagerTLOU(stop, dayDuration, countMutexTLOU, stateMutexTLOU);
+        pmTLOU.start();
+        
     }
 
     /**
@@ -139,6 +154,10 @@ public class Dashboard extends javax.swing.JFrame {
         numPlotTLOU = new javax.swing.JLabel();
         jLabel7 = new javax.swing.JLabel();
         numChapters = new javax.swing.JLabel();
+        jLabel8 = new javax.swing.JLabel();
+        pmState = new javax.swing.JLabel();
+        daysCounter = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -154,40 +173,54 @@ public class Dashboard extends javax.swing.JFrame {
 
         jLabel7.setText("Ensamblador:");
 
+        jLabel8.setText("Project Manager:");
+
+        jLabel9.setText("Contador:");
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap(201, Short.MAX_VALUE)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jLabel7)
-                    .addComponent(jLabel6)
-                    .addComponent(jLabel5)
-                    .addComponent(jLabel4)
-                    .addComponent(jLabel3)
-                    .addComponent(jLabel2))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+            .addGroup(layout.createSequentialGroup()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(numIntroTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numCreditTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numBegTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numEndTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numPlotTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(numChapters, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addGap(34, 34, 34))
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel7, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel6, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel5, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel3, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel2, javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(jLabel9, javax.swing.GroupLayout.Alignment.TRAILING))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(numIntroTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(numCreditTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(numBegTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(numEndTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(numPlotTLOU, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(numChapters, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(daysCounter, javax.swing.GroupLayout.PREFERRED_SIZE, 121, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(36, 36, 36)
+                        .addComponent(jLabel8)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addComponent(pmState, javax.swing.GroupLayout.PREFERRED_SIZE, 270, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 0, Short.MAX_VALUE)))
+                .addGap(27, 27, 27))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(49, 49, 49)
+                .addGap(22, 22, 22)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(numIntroTLOU, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE))
+                    .addComponent(jLabel3, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(layout.createSequentialGroup()
-                        .addGap(0, 1, Short.MAX_VALUE)
+                        .addGap(0, 0, Short.MAX_VALUE)
                         .addComponent(jLabel2, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
                     .addComponent(numCreditTLOU, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
@@ -206,7 +239,17 @@ public class Dashboard extends javax.swing.JFrame {
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(jLabel7, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(numChapters, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addContainerGap(64, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jLabel9, javax.swing.GroupLayout.PREFERRED_SIZE, 26, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addComponent(daysCounter, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(pmState, javax.swing.GroupLayout.DEFAULT_SIZE, 26, Short.MAX_VALUE)
+                    .addComponent(jLabel8, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(32, 32, 32))
         );
 
         pack();
@@ -248,17 +291,21 @@ public class Dashboard extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    public static javax.swing.JLabel daysCounter;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
     private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel8;
+    private javax.swing.JLabel jLabel9;
     public static javax.swing.JLabel numBegTLOU;
     public static javax.swing.JLabel numChapters;
     public static javax.swing.JLabel numCreditTLOU;
     public static javax.swing.JLabel numEndTLOU;
     public static javax.swing.JLabel numIntroTLOU;
     public static javax.swing.JLabel numPlotTLOU;
+    public static javax.swing.JLabel pmState;
     // End of variables declaration//GEN-END:variables
 }
